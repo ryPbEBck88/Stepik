82485c873e4befbf521123e0ae64097562aec978
Alexandr Khalupa <AlexandrKhalupa@yandex.ru>
2024-06-13 14:07:11 +0300

    Использовал цикл, кортеж, список, getattr.

---
+++ introduction_to_classes/attributes_class/2.py
@@ -0,0 +1,15 @@
+"""
+Перед нами все тот же класс Book, 
+но теперь ваша задача получить доступ 
+к атрибутам writer и pages при помощи функции getattr
+Сначала выведите атрибут writer, затем на новой строке pages
+"""
+
+# Дано по заданию.
+class Book:
+    name = '1984'
+    writer = 'Джордж Оруэлл'
+    pages = 213
+
+# Мое решение
+print(*[getattr(Book, attr) for attr in ('writer', 'name')], sep='\n')
