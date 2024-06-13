2c3f4bfeb8d23d07e165a1c1a3c3cb1774fe45c5
Alexandr Khalupa <AlexandrKhalupa@yandex.ru>
2024-06-13 19:58:22 +0300

    Функция hasattr.

--- introduction_to_classes/attributes_class/8.py
+++ introduction_to_classes/attributes_class/8.py
@@ -4,5 +4,5 @@
 Ваша задача написать только определение функции is_obj_has_attr
 """
 
-def is_obj_has_attr(class_name, attr_class):
+def is_obj_has_attr(class_name, attr_class)  -> bool:
     return hasattr(class_name, attr_class)
